70f95a54688d6368cdc81c2d80a5c8081cd8f195
Kiatisak Pethor <kiatisak.p@mail.kmutt.ac.th>
2020-10-16 10:25:44 +0700

    [Update] Change paramerter and remove unused comment

--- app.py
+++ app.py
@@ -9,51 +9,7 @@ fed.shapeDetector('../pillbox_production_images_full_201812/SUN01450.jpg')
 fed.shapeDetector('../pillbox_production_images_full_201812/SUN05550.jpg')
 fed.shapeDetector('../pillbox_production_images_full_201812/AUR08410.jpg')
 fed.shapeDetector('../pillbox_production_images_full_201812/00006-0960-54_C31361AB.jpg')
-# fed.show_image(mask_org)
-# # blurred = cv2.GaussianBlur(gray, (5,5), 0 )
-# th1 = cv2.threshold(gray, 165,255,cv2.THRESH_BINARY)[1]
-#
-#
-# plt.imshow(th1, cmap='gray', interpolation='bicubic')
-# # plt.xticks([]), plt.yticks([]) # to hide tick values on X and Y axis
-# plt.show()
-#
-# contours = cv2.findContours(th1, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_KCOS)
-#
-# contours = imutils.grab_contours(contours) # where cnts is the variable in which contours are stored, replace it with your variable name
-# contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10] # sorts contours according to their area from largest to smallest.
-# area = cv2.contourArea(contours[0])
-# largestCont = contours[0] #store the largest contour
-# area = cv2.contourArea(largestCont)
-# print(largestCont)
-# print(area)
-#
-# cv2.drawContours(circle_img, largestCont, -1, (0, 0, 255), 5)
-# plt.imshow(circle_img, cmap='gray', interpolation='bicubic')
-# # plt.xticks([]), plt.yticks([]) # to hide tick values on X and Y axis
-# plt.show()
-# plt.close()
-# if it is circle len largest contour is more than 4
-
-
-# for c in contours:
-#    # rect = cv2.boundingRect(c)
-#    # x,y,w,h = rect
-#    # area = w * h
-#    area = cv2.contourArea(c)
-#    epsilon = 0.08 * cv2.arcLength(c, True)
-#    approx = cv2.approxPolyDP(c, epsilon, True)
-#    print(area)
-# # กำหนด Area ที่จะทำการ Draw Contour
-# if area > 500:
-#     cv2.drawContours(imgs, [approx], -1, (0, 0, 255), 5)
-#     # cv2.rectangle(imgs, (x, y), (x+w, y+h), (0, 255, 0), 5)
-#     print('approx', approx)
-#     print('1')
-#     # for x in range(0, len(approx)):
-#     #     cv2.circle(imgs, (approx[x][0][0], approx[x][0][1]), 30, (0,0,255), -1)
-#
-# plt.imshow(imgs, cmap='gray', interpolation='bicubic')
-# # plt.xticks([]), plt.yticks([]) # to hide tick values on X and Y axis
-# plt.show()
-# plt.close()
+fed.shapeDetector('../pillbox_production_images_full_201812/2be62df6-0287-50ba-e054-00144ff88e88.jpg')
+fed.shapeDetector('../pillbox_production_images_full_201812/2e92d053-1618-143b-e054-00144ff8d46c.jpg')
+fed.shapeDetector('../pillbox_production_images_full_201812/3mg_Capsule.jpg')
+fed.shapeDetector('../pillbox_production_images_full_201812/00004-0259-01_B8135C6A.jpg')
--- fed.py
+++ fed.py
@@ -8,6 +8,7 @@ def load_image(path_img):
     img2 = imutils.resize(img, width=360)
     return img2
 
+
 def show_image(img):
     plt.imshow(img, cmap='gray', interpolation='bicubic')
     # plt.xticks([]), plt.yticks([]) # to hide tick values on X and Y axis
@@ -16,25 +17,29 @@ def show_image(img):
 
 
 def cvt2gray(img):
-    return cv2.cvtColor(img , cv2.COLOR_BGR2GRAY)
+    return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
-def create_mask(img,number):
+def create_mask(img, number):
     # return cv2.threshold(img,number,255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    return cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,17,3.5)
+    return cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 27, 3)
 
 
 def largest_contour(mask):
-    contours = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_TC89_L1) #cv.CHAIN_APPROX_SIMPLE,CHAIN_APPROX_TC89_KCOS
-    contours = imutils.grab_contours(contours) # where cnts is the variable in which contours are stored, replace it with your variable name
-    contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10] # sorts contours according to their area from largest to smallest.
-    largestCont = contours[1] #store the largest contour
+    contours = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_TC89_L1)
+    # cv.CHAIN_APPROX_SIMPLE,CHAIN_APPROX_TC89_KCOS
+    contours = imutils.grab_contours(contours)
+    # where cnts is the variable in which contours are stored, replace it with your variable name
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+    # sorts contours according to their area from largest to smallest.
+    largestCont = contours[2]  # store the largest contour
     area = cv2.contourArea(largestCont)
-    print('area = ' , area)
+    print('area = ', area)
     return largestCont
 
-def drawing_cont(img,contour):
-    epsilon = 0.085 * cv2.arcLength(contour, True)
+
+def drawing_cont(img, contour):
+    epsilon = 0.037 * cv2.arcLength(contour, True)
     approx = cv2.approxPolyDP(contour, epsilon, True)
     print(len(approx))
     cv2.drawContours(img, approx, -1, (255, 0, 255), 5)
@@ -44,14 +49,15 @@ def drawing_cont(img,contour):
 def shapeDetector(path_img):
     img = load_image(path_img)
     gray = cvt2gray(img)
-    mask = create_mask(gray,0)
+    mask = create_mask(gray, 0)
     # show_image(mask)
     cont = largest_contour(mask)
     # we use the second largest because normally largest are box of the picture
-    approx = drawing_cont(img,cont)
+    approx = drawing_cont(img, cont)
     show_image(img)
     shapePred(approx)
     pass
+
 
 def shapePred(approx):
     if len(approx) == 2:
@@ -59,5 +65,3 @@ def shapePred(approx):
     if len(approx) == 4:
         print('shape is Circle or square')
     pass
-
-
